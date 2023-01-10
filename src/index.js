5e2f95410d832602f4c18db23f3dca0e1eb991f0
Olga Birkovska <birkovskaya92@ukr.net>
2023-01-10 20:11:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
-import { Loader } from 'components/Loader/Loader';
 import { store } from 'redux/store';
 import './index.css';
 
