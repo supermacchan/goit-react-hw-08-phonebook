3ab8511c13c503c041a65d51fc57b98ed130119a
Olga Birkovska <birkovskaya92@ukr.net>
2023-01-10 19:38:37 +0200

    slice

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,7 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import { toast } from 'react-toastify';
 import { fetchContacts } from "./operations";
+
+const handlePending = state => {
+  state.isLoading = true;
+};
+
+const handleRejected = (state, action) => {
+  state.isLoading = false;
+  state.error = action.payload;
+};
 
 const contactSlice = createSlice({
   name: "contacts",
@@ -11,56 +20,64 @@ const contactSlice = createSlice({
     error: null
   },
 
-  reducers: {
-    addContact: {
-      reducer(state, action) {
-        const contactsNames = state.items.map(contact => contact.name);
-        if(contactsNames.includes(action.payload.name)) {
-          toast.error(`${action.payload.name} is already in contacts.`);
-          return;
-        }
-        state.items.unshift(action.payload);
-      },
-      prepare(name, number) {
-        return {
-          payload: {
-            id: nanoid(),
-            name,
-            number,
-          },
-        };
-      },
-    },
-    deleteContact(state, action) {
-      const index = state.items.findIndex(contact => contact.id === action.payload);
-      state.items.splice(index, 1);
-    },
-  },
+  // reducers: {
+  //   addContact: {
+  //     reducer(state, action) {
+  //       const contactsNames = state.items.map(contact => contact.name);
+  //       if(contactsNames.includes(action.payload.name)) {
+  //         toast.error(`${action.payload.name} is already in contacts.`);
+  //         return;
+  //       }
+  //       state.items.unshift(action.payload);
+  //     },
+  //     prepare(name, number) {
+  //       return {
+  //         payload: {
+  //           id: nanoid(),
+  //           name,
+  //           number,
+  //         },
+  //       };
+  //     },
+  //   },
+  //   deleteContact(state, action) {
+  //     const index = state.items.findIndex(contact => contact.id === action.payload);
+  //     state.items.splice(index, 1);
+  //   },
+  // },
 
   extraReducers: {
-    [fetchContacts.pending](state) {
-      state.isLoading = true;
-    },
+    [fetchContacts.pending]: handlePending,
     [fetchContacts.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
       state.items = action.payload;
     },
-    [fetchContacts.rejected](state, action) {
+    [fetchContacts.rejected]: handleRejected,
+
+    [addContact.pending]: handlePending,
+    [addContact.fulfilled](state, action) {
       state.isLoading = false;
-      state.error = action.payload;
+      state.error = null;
+      const contactsNames = state.items.map(contact => contact.name);
+      if(contactsNames.includes(action.payload.name)) {
+        toast.error(`${action.payload.name} is already in contacts.`);
+        return;
+      }
+      state.items.unshift(action.payload);
     },
+    [addContact.rejected]: handleRejected,
+
+    [deleteContact.pending]: handlePending,
+    [deleteContact.fulfilled](state, action) {
+      state.isLoading = false;
+      state.error = null;
+      const index = state.items.findIndex(contact => contact.id === action.payload);
+      state.items.splice(index, 1);
+    },
+    [deleteContact.rejected]: handleRejected,
   }
 });
 
 export const { addContact, deleteContact } = contactSlice.actions;
 export const contactsReducer = contactSlice.reducer;
-
-// {
-//   contacts: {
-//     items: [],
-//     isLoading: false,
-//     error: null
-//   },
-//   filter: ""
-// }
--- src/redux/operations.js
+++ src/redux/operations.js
@@ -5,9 +5,13 @@ axios.defaults.baseURL = "https://63bd96dabc99130c6cf784db.mockapi.io";
 
 export const fetchContacts = createAsyncThunk(
     "contacts/fetchAll",
-    async () => {
-        const response = await axios.get("/contacts");
-        return response.data;
+    async (_, thunkAPI) => {
+        try {
+            const response = await axios.get("/contacts");
+            return response.data;
+        } catch (error) {
+            return thunkAPI.rejectWithValue(error.message);
+        }
     }
 );
 
