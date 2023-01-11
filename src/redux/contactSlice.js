d0a0ca3c3c115420ebb4bfd2a1224260f469e757
Olga Birkovska <birkovskaya92@ukr.net>
2023-01-11 13:50:40 +0200

    fix deleting

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -40,7 +40,7 @@ const contactSlice = createSlice({
     [deleteContact.fulfilled](state, action) {
       state.isLoading = false;
       state.error = null;
-      const index = state.items.findIndex(contact => contact.id === action.payload);
+      const index = state.items.findIndex(contact => contact.id === action.payload.id);
       state.items.splice(index, 1);
     },
     [deleteContact.rejected]: handleRejected,
