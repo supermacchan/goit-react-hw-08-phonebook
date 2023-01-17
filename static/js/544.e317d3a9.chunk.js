b17a95e6b34278e670a0bb24af70598c30630ff6
supermacchan <aiba.aki9@gmail.com>
2023-01-17 21:49:40 +0000

    Deploying to gh-pages from @ supermacchan/goit-react-hw-08-phonebook@5e12a1bf8bcb89079be5700d9aa94d8320ed610a 🚀

---
+++ static/js/544.e317d3a9.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[544],{9544:function(e,o,t){t.r(o);var n=t(184);o.default=function(){return(0,n.jsx)("div",{children:"You are at home"})}}}]);
+//# sourceMappingURL=544.e317d3a9.chunk.js.map
