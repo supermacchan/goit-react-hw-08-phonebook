eacd0a624dc658862cf5ef0dc30e1f1fc8495539
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-17 19:09:37 +0200

    redux-persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,41 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactSlice";
 import { filtersReducer } from "./filterSlice";
+import storage from 'redux-persist/lib/storage';
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+  } from 'redux-persist';
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
+
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['filter'],
+    stateReconciler: autoMergeLevel2,
+};
 
 const rootReducer = combineReducers({
     contacts: contactsReducer,
     filter: filtersReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const store = configureStore({
-    reducer: rootReducer,
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => 
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+              },
+        })
 });
+
+export const persistor = persistStore(store);
