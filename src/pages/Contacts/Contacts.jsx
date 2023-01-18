e7980eae3c7f955331b910023011f0148642336f
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 23:21:16 +0200

    Update Contacts.jsx

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,17 +1,26 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
 import { ContactForm } from "../../components/ContactForm/ContactForm";
 import { Filter } from "../../components/Filter/Filter";
 import { ContactList } from "../../components/ContactList/ContactList";
+import { LoggedOut } from 'components/LoggedOut/LoggedOut';
 import css from './Contacts.module.css';
 
 const Contacts = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
         <>
-            <h1 className={css.phonebook__title}>Phonebook</h1>
-            <ContactForm />
+            {isLoggedIn ? 
+            <>
+                <h1 className={css.phonebook__title}>Phonebook</h1>
+                <ContactForm />
 
-            <h2 className={css.contacts__title}>Contacts</h2>
-            <Filter />
-            <ContactList />
+                <h2 className={css.contacts__title}>Contacts</h2>
+                <Filter />
+                <ContactList />
+            </>
+            : <LoggedOut />}
         </>
     )
 };
