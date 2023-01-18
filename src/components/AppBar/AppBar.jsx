3710469a86dbac7b0688a31fd133ab577d23224c
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 19:13:43 +0200

    navigation

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,18 +1,12 @@
 import { NavLink } from "react-router-dom";
-import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import { Navigation } from "components/Navigation/Navigation";
 import css from "./AppBar.module.css";
 
 export const AppBar = () => {
     return (
         <header className={css.header}>
             <nav className={css.header__nav}>
-                <NavLink to="/" className={css.header__link}>
-                    <BsFillJournalBookmarkFill className={css.header__icon} />
-                    Home
-                </NavLink>
-                <NavLink to={"/contacts"} className={css.header__link}>
-                    Contacts
-                </NavLink>
+                <Navigation />
                 <NavLink to={"/register"} className={css.header__link}>
                     Register
                 </NavLink>
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import { BsFillJournalBookmarkFill } from 'react-icons/bs';
+import css from "./Navigation.module.css";
+
+export const Navigation = () => {
+    return (
+        <>
+            <NavLink to="/" className={css.header__link}>
+                <BsFillJournalBookmarkFill className={css.header__icon} />
+                Home
+            </NavLink>
+            <NavLink to={"/contacts"} className={css.header__link}>
+                Contacts
+            </NavLink>    
+        </>
+    );
+}
