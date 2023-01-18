1e4fae14824af3c6fd25cd5599b22ba20ffc352f
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 22:33:34 +0200

    login success

--- src/components/LoginSuccess/LoginSuccess.jsx
+++ src/components/LoginSuccess/LoginSuccess.jsx
@@ -1,13 +1,17 @@
 import { useSelector } from 'react-redux';
 import { selectUserName } from 'redux/auth/authSelectors';
+import { Link } from 'react-router-dom';
 import css from './LoginSuccess.module.css';
 
 export const LoginSuccess = () => {
     const name = useSelector(selectUserName);
+    
     return (
         <div className={css.success}>
-            <h2 className={css.title}>Welcome back, {name}!</h2>
-            
+            <h2 className={css.title}>Welcome, {name}!</h2>
+            <p className={css.notification}>
+                You can now go to your <Link to={"/contacts"} className={css.link}>Contact list</Link>.
+            </p>
         </div>
     )
 }
