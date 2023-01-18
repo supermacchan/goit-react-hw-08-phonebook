28f58bd6087daf71c95576bcc41976154097682e
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 18:59:00 +0200

    home styling (for start)

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,9 @@
+import css from './Home.module.css';
+
 const Home = () => {
     return (
         <div>
-            <h1>Welcome Home</h1>
+            <h1 className={css.title}>Welcome Home</h1>
         </div>
     )
 };
