3d0239d8f653e39ef7194a53bd2ed0ab88b468bf
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 20:44:19 +0200

    Update LogIn.jsx

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,8 +1,28 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'redux/auth/authOperations';
+
 import { HiOutlineMail } from 'react-icons/hi';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import css from './LogIn.module.css';
 
 const LogIn = () => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChange = event => {
+        const { name, value } = event.currentTarget;
+        switch (name) {
+            case 'email':
+                return setEmail(value);
+            case 'password': 
+                return setPassword(value);
+            default:
+                return;
+        }
+    };
+
     return (
         <section className={css.login}>
             <h2 className={css.title}>Sign in below</h2>
@@ -21,6 +41,8 @@ const LogIn = () => {
                             id="emailInputId"
                             required
                             className={css.input}
+                            value={email}
+                            onChange={handleChange}
                         />
                     </li>
                     <li className={css.listItem}>
@@ -33,11 +55,11 @@ const LogIn = () => {
                         <input
                             type="password"
                             name="password"
-                            // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-                            // title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
                             id="passwordInputId"
                             required
                             className={css.input}
+                            value={password}
+                            onChange={handleChange}
                         />
                     </li>
                 </ul>
