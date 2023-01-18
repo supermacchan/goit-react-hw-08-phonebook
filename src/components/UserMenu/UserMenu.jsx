2286e08173f690aac7431f184b942af17951fa61
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 20:58:55 +0200

    Update UserMenu.jsx

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,9 +1,12 @@
+import { useSelector } from 'react-redux';
+import { selectUserName } from 'redux/auth/authSelectors';
 import css from "./UserMenu.module.css";
 
 export const UserMenu = () => {
+    const name = useSelector(selectUserName);
     return (
         <div className={css.userMenu}>
-            <p className={css.userName}>Hello, User!</p>
+            <p className={css.userName}>Hello, {name}!</p>
             <button type="button" className={css.logOutBtn}>Log Out</button>
         </div>
     );
