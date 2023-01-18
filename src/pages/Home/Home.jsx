813419d53ed674278dd5c03d8f1426bf46153fee
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 22:36:14 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
+import { LoginSuccess } from 'components/LoginSuccess/LoginSuccess';
 import css from './Home.module.css';
 
 const Home = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
-        <div>
-            <h1 className={css.title}>Welcome Home</h1>
-        </div>
+        <>
+            {isLoggedIn ? <LoginSuccess /> :
+            <div>
+                <h1 className={css.title}>Welcome Home</h1>
+            </div>}
+        </> 
     )
 };
 
