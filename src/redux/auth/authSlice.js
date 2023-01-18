f0bd75f6449e683f34407a5318f6da668d007c4d
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 22:06:33 +0200

    Update authSlice.js

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -20,16 +20,13 @@ const authSlice = createSlice({
             state.isLoggedIn = true;
         },
         [authOperations.register.pending](state, action) {
-            return state;
+
         },
         [authOperations.register.rejected](state, action) {
-            console.log(action);
-            console.log(state);
+            
         },
 
         [authOperations.logIn.fulfilled](state, action) {
-            console.log(state);
-            console.log(action.payload);
             state.user = action.payload.user;
             state.token = action.payload.token;
             state.isLoggedIn = true;
@@ -38,8 +35,7 @@ const authSlice = createSlice({
             
         },
         [authOperations.logIn.rejected](state, action) {
-            console.log(action);
-            console.log(state);
+            
         },
 
         [authOperations.logOut.fulfilled](state, action) {
