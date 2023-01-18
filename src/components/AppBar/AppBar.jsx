da42083bee115545eca3c83670ee631503341b8f
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 20:57:45 +0200

    is logged in

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,16 +1,19 @@
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
 import { Navigation } from "components/Navigation/Navigation";
 import { AuthNav } from "components/AuthNav/AuthNav";
 import { UserMenu } from "components/UserMenu/UserMenu";
 import css from "./AppBar.module.css";
 
 export const AppBar = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
         <header className={css.header}>
             <nav className={css.header__nav}>
                 <Navigation />
             </nav>
-            <AuthNav />
-            <UserMenu />
+            {isLoggedIn ? <UserMenu /> : <AuthNav />}
         </header>
     );
 }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
 import { BsFillJournalBookmarkFill } from 'react-icons/bs';
 import css from "./Navigation.module.css";
 
 export const Navigation = () => {
+    const isLoggedIn = useSelector(selectIsLoggedIn);
+
     return (
         <>
             <NavLink to="/" className={css.header__link}>
                 <BsFillJournalBookmarkFill className={css.header__icon} />
                 Home
             </NavLink>
-            {/* добавить условия отображения контактов (при логине) */}
-            <NavLink to={"/contacts"} className={css.header__link}>
-                Contacts
-            </NavLink>    
+            {isLoggedIn && 
+                <NavLink to={"/contacts"} className={css.header__link}>
+                    Contacts
+                </NavLink>  
+            }       
         </>
     );
 }
