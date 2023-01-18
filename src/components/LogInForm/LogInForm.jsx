74f32309f9ec591514380d1a52bd6a27515bf229
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 23:04:01 +0200

    LogInForm

---
+++ src/components/LogInForm/LogInForm.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { authOperations } from 'redux/auth/authOperations';
+import { HiOutlineMail } from 'react-icons/hi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import css from './LogInForm.module.css';
+
+export const LogInForm = () => {
+    const dispatch = useDispatch();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleChange = event => {
+        const { name, value } = event.currentTarget;
+        switch (name) {
+            case 'email':
+                return setEmail(value);
+            case 'password': 
+                return setPassword(value);
+            default:
+                return;
+        }
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        dispatch(authOperations.logIn({ email, password }));
+        reset();
+    };
+
+    const reset = () => {
+        setEmail('');
+        setPassword('');
+    };
+
+    return (
+        <>
+            <h2 className={css.title}>Sign in below</h2>
+            <form className={css.logInForm} onSubmit={handleSubmit}>
+                <ul className={css.list}>
+                    <li className={css.listItem}>
+                        <label
+                            htmlFor="emailInputId"
+                            className={css.label}
+                        >
+                            <HiOutlineMail className={css.icon} /> Email
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="emailInputId"
+                            required
+                            className={css.input}
+                            value={email}
+                            onChange={handleChange}
+                        />
+                    </li>
+                    <li className={css.listItem}>
+                        <label
+                            htmlFor="passwordInputId"
+                            className={css.label}
+                        >
+                            <RiLockPasswordLine className={css.icon} /> Password
+                        </label>
+                        <input
+                            type="password"
+                            name="password"
+                            id="passwordInputId"
+                            required
+                            className={css.input}
+                            value={password}
+                            onChange={handleChange}
+                        />
+                    </li>
+                </ul>
+                <button type='submit' className={css.submitBtn}>Sign In</button>
+            </form>
+        </>
+    );
+}
--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,85 +1,15 @@
-import { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/authSelectors';
-import { authOperations } from 'redux/auth/authOperations';
 import { LoginSuccess } from 'components/LoginSuccess/LoginSuccess';
-import { HiOutlineMail } from 'react-icons/hi';
-import { RiLockPasswordLine } from 'react-icons/ri';
+import { LogInForm } from 'components/LogInForm/LogInForm';
 import css from './LogIn.module.css';
 
 const LogIn = () => {
-    const dispatch = useDispatch();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
     const isLoggedIn = useSelector(selectIsLoggedIn);
-
-    const handleChange = event => {
-        const { name, value } = event.currentTarget;
-        switch (name) {
-            case 'email':
-                return setEmail(value);
-            case 'password': 
-                return setPassword(value);
-            default:
-                return;
-        }
-    };
-
-    const handleSubmit = event => {
-        event.preventDefault();
-        dispatch(authOperations.logIn({ email, password }));
-        reset();
-    };
-
-    const reset = () => {
-        setEmail('');
-        setPassword('');
-    };
 
     return (
         <section className={css.login}>
-            {isLoggedIn ? <LoginSuccess /> : <>
-            <h2 className={css.title}>Sign in below</h2>
-            <form className={css.logInForm} onSubmit={handleSubmit}>
-                <ul className={css.list}>
-                    <li className={css.listItem}>
-                        <label
-                            htmlFor="emailInputId"
-                            className={css.label}
-                        >
-                            <HiOutlineMail className={css.icon} /> Email
-                        </label>
-                        <input
-                            type="email"
-                            name="email"
-                            id="emailInputId"
-                            required
-                            className={css.input}
-                            value={email}
-                            onChange={handleChange}
-                        />
-                    </li>
-                    <li className={css.listItem}>
-                        <label
-                            htmlFor="passwordInputId"
-                            className={css.label}
-                        >
-                            <RiLockPasswordLine className={css.icon} /> Password
-                        </label>
-                        <input
-                            type="password"
-                            name="password"
-                            id="passwordInputId"
-                            required
-                            className={css.input}
-                            value={password}
-                            onChange={handleChange}
-                        />
-                    </li>
-                </ul>
-                <button type='submit' className={css.submitBtn}>Sign In</button>
-            </form>
-            </>}
+            {isLoggedIn ? <LoginSuccess /> : <LogInForm />}
         </section>
     )
 };
