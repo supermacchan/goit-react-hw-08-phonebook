64d65ff30adfdb0652f672e97727e926b1025326
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 22:50:12 +0200

    home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 import { selectIsLoggedIn } from 'redux/auth/authSelectors';
 import { LoginSuccess } from 'components/LoginSuccess/LoginSuccess';
 import { LoggedOut } from 'components/LoggedOut/LoggedOut';
-// import css from './Home.module.css';
 
 const Home = () => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
