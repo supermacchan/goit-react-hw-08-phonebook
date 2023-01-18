75f6f3bf41dae3afd29faee85e4a98fd081196c1
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-18 18:54:29 +0200

    Update LogIn.jsx

--- src/pages/LogIn/LogIn.jsx
+++ src/pages/LogIn/LogIn.jsx
@@ -1,42 +1,49 @@
+import { HiOutlineMail } from 'react-icons/hi';
+import { RiLockPasswordLine } from 'react-icons/ri';
 import css from './LogIn.module.css';
 
 const LogIn = () => {
     return (
-        <>
+        <section className={css.login}>
             <h2 className={css.title}>Sign in below</h2>
             <form className={css.logInForm}>
-                <label
-                    htmlFor="emailInputId"
-                    className={css.label}
-                >
-                    Email
-                </label>
-                <input
-                    type="email"
-                    name="email"
-                    id="emailInputId"
-                    required
-                    className={css.input}
-                />
-
-                <label
-                    htmlFor="passwordInputId"
-                    className={css.label}
-                >
-                    Password
-                </label>
-                <input
-                    type="text"
-                    name="email"
-                    // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
-                    // title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
-                    id="passwordInputId"
-                    required
-                    className={css.input}
-                />
+                <ul className={css.list}>
+                    <li className={css.listItem}>
+                        <label
+                            htmlFor="emailInputId"
+                            className={css.label}
+                        >
+                            <HiOutlineMail className={css.icon} /> Email
+                        </label>
+                        <input
+                            type="email"
+                            name="email"
+                            id="emailInputId"
+                            required
+                            className={css.input}
+                        />
+                    </li>
+                    <li className={css.listItem}>
+                        <label
+                            htmlFor="passwordInputId"
+                            className={css.label}
+                        >
+                            <RiLockPasswordLine className={css.icon} /> Password
+                        </label>
+                        <input
+                            type="text"
+                            name="email"
+                            // pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" 
+                            // title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters"
+                            id="passwordInputId"
+                            required
+                            className={css.input}
+                        />
+                    </li>
+                </ul>
                 <button type='submit' className={css.logInBtn}>Sign In</button>
             </form>
-        </>
+        </section>
     )
 };
 
