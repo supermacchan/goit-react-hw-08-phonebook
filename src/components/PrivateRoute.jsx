7a3521d0d2e5d0072d05e1ca46f3b2ac584f02d3
Olga Birkovska <aiba.aki9@gmail.com>
2023-01-19 23:43:10 +0200

    redirect

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -3,12 +3,16 @@ import { Route, Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { selectIsLoggedIn } from "redux/auth/authSelectors";
 
-export const PrivateRoute = ({ children, ...routeProps }) => {
+export const PrivateRoute = ({ 
+    children, 
+    redirectTo = '/login', 
+    ...routeProps 
+}) => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
 
     return (
         <Route {...routeProps}>
-            { isLoggedIn ? children : <Navigate to="/login" /> }
+            { isLoggedIn ? children : <Navigate to={redirectTo} /> }
         </Route>
     );
 };
@@ -16,5 +20,6 @@ export const PrivateRoute = ({ children, ...routeProps }) => {
 PrivateRoute.propTypes = {
     exact: PropTypes.bool,
     path: PropTypes.string.isRequired,
+    redirectTo: PropTypes.string.isRequired,
     children: PropTypes.node,
 }
--- src/components/PublicRoute.jsx
+++ src/components/PublicRoute.jsx
@@ -3,7 +3,12 @@ import { Route, Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 import { selectIsLoggedIn } from "redux/auth/authSelectors";
 
-export const PublicRoute = ({ children, restricted = false, ...routeProps }) => {
+export const PublicRoute = ({ 
+    children, 
+    restricted = false, 
+    redirectTo = '/contacts', 
+    ...routeProps 
+}) => {
     const isLoggedIn = useSelector(selectIsLoggedIn);
     const shouldRedirect = isLoggedIn && restricted;
     return (
@@ -17,5 +22,6 @@ PublicRoute.propTypes = {
     exact: PropTypes.bool,
     path: PropTypes.string.isRequired,
     restricted: PropTypes.bool,
+    redirectTo: PropTypes.string.isRequired,
     children: PropTypes.node,
 }
